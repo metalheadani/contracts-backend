759a3d6f186fdd860c6b1e4d9f359e777eb0c965
mrrobot <anirudhajain.910@gmail.com>
2018-11-02 01:47:04 +0530

    user models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4&9h$&7v2tod_bjjjfitxh*t8@by!jbj3jo=ee%_f!&r1kk#@u'
+SECRET_KEY = '' #add your own key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
